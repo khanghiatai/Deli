54b11627a8795a00ac99cae5d9dd0ae9e56aa0db
khanghiatai <khanghiatai@gmail.com>
2017-10-04 18:05:39 +0700

    10/4/2017

---
+++ src/modules/TestNG.java
@@ -0,0 +1,44 @@
+package modules;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class TestNG {
+	WebDriver driver;
+	@BeforeTest
+	public void a() {
+		System.setProperty("webdriver.chrome.driver", "F:\\Project\\project\\libs\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--start-maximized");
+		driver = new ChromeDriver(options);
+		driver.get("http://sandbox.deliverynow.vn:8081/ho-chi-minh/highland-coffee-tran-hung-dao-26728");
+	}
+	
+	@Test
+	public void f(){
+		driver.findElement(By.xpath(".//*[@ng-show='detailCtrl.isHost' and @class='btn-book-first']")).click();
+		
+		/*List<WebElement> el = (List<WebElement>) ((JavascriptExecutor)driver)
+				.executeScript("return document.querySelectorAll('.more-info .adding-food-cart .btn-adding');");
+				
+		for (WebElement webElement : el) {
+			System.out.println("c1");
+		}*/
+
+	}
+	
+	@AfterTest
+	public void c() {
+		System.out.println("c");
+	}
+}
