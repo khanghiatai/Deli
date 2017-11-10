e2077cfad6b8ced764e3e871f72c59c84f1622ad
khanghiatai <khanghiatai@gmail.com>
2017-11-10 17:55:14 +0700

    10/11/2017

--- src/configuration/ResourceHasMap.java
+++ src/configuration/ResourceHasMap.java
@@ -51,6 +51,7 @@ public class ResourceHasMap {
 		hm.put("contentsuccess", "Cảm ơn bạn đã sử dụng dịch vụ của deliveryNow.vn! Đơn hàng của bạn đang được chúng tôi xử lý.");
 		hm.put("historyorder", "Lịch sử đặt món");
 		hm.put("waitconfirm", "Chờ xác nhận");
+
 	}
 	
 	private void dayOffWeek(){
